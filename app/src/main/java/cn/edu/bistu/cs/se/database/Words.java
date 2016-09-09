88def87b9b8cf5810aeb315c266011cd6b2f6585
LSYing <312458788@qq.com>
2016-09-09 23:09:43 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/database/Words.java
@@ -0,0 +1,18 @@
+package cn.edu.bistu.cs.se.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by lsy on 2016/9/8.
+ */
+public class Words {
+    public Words() {
+    }
+
+    public static abstract class Word implements BaseColumns {
+        public static final String TABLE_NAME="words";
+        public static final String COLUMN_NAME_WORD="word";//列：单词
+        public static final String COLUMN_NAME_MEANING="meaning";//列：单词含义
+        public static final String COLUMN_NAME_SAMPLE="sample";//单词示例
+    }
+}
